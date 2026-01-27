0b4e93f20decdb1990f5d0529f40a4bb0db2f20b
Mayur Kumavat <mkmayur4480@gmail.com>
2026-01-27 17:39:40 +0530

    mk

--- src/components/HeroLeft.jsx
+++ src/components/HeroLeft.jsx
@@ -11,7 +11,7 @@ const HeroLeft = () => {
   };
   return (
     <div className='flex justify-center lg:justify-center items-start lg:items-center mt-16 lg:-mt-16 flex-col w-full lg:min-h-screen min-h-[70vh] pr-2 '>
-        <h3 className='text-lime-500 uppercase text-lg lg:text-2xl font-semibold'>welcome to Gayatri Solar</h3>
+        <h3 className='text-lime-500 uppercase text-lg lg:text-2xl font-semibold'>mk welcome to Gayatri Solar</h3>
         <h2 className='text-3xl lg:text-7xl font-bold'>Lighting Lives with</h2>
         <h2 className='text-3xl lg:mt-2 lg:text-7xl text-nowrap font-bold text-lime-500'>Smart Solar Solutions</h2>
         <p className=' lg:text-xl text-lg py-3 lg:w-[70%] lg:text-center capitalize text-white'>Gayatri Solar offers advanced solar energy solutions for residential, industrial, and rural projects. We focus on efficient, eco-friendly systems that reduce energy costs and promote a cleaner, sustainable future.</p>
